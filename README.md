05bf76fbe2112df8508ef352cdd352555153bc3e
Yan Zhengyang <banixc@qq.com>
2017-06-05 08:53:03 +0800

    参数转换

--- README.md
+++ README.md
@@ -30,3 +30,12 @@
 
 #### cutFrameNumber
 要截取的图片数量
+
+## stereo.yml
+该文件通过附带的
+matlab/m2y2.m matlab程序生成
+m2y2.m的工作为读取matlab的标定结果 *.mat 文件 并将其转换为yml文件
+其中标定的结果文件通过matlab的双目标定应用 Stereo Camera Calibrator 进行标定，标定的结果导出后再保存为.mat文件
+
+  load('matlab.mat'); %加载标定的结果文件%
+  dym_matlab2opencv(); %将matlab文件转换为opencv能识别的格式并写入到文件里%
--- matlab/m2y2.m
+++ matlab/m2y2.m
@@ -1,6 +1,8 @@
 function m2y2()
+% 加载标定的结果文件 %
 load('matlab.mat');
 
+% 读取并进行参数预处理 %
 cameraMatrix1 = stereoParams.CameraParameters1.IntrinsicMatrix';
 cameraMatrix2 = stereoParams.CameraParameters2.IntrinsicMatrix';
 distCoeffs1 = [stereoParams.CameraParameters1.RadialDistortion,stereoParams.CameraParameters1.TangentialDistortion,0]';
@@ -8,6 +10,7 @@ distCoeffs2 = [stereoParams.CameraParameters2.RadialDistortion,stereoParams.Came
 R = stereoParams.RotationOfCamera2';
 T = stereoParams.TranslationOfCamera2';
 
+% 将matlab文件转换为opencv能识别的格式并写入到文件里 %
 filename = 'stereo.yml';
 dym_matlab2opencv( cameraMatrix1, filename);
 dym_matlab2opencv( cameraMatrix2, filename, 'a');
